d1c5f337f90e6f564c2edc936823f2b4c41bca8f
chefking <chefking9000@protonmail.com>
2022-01-15 16:02:41 +0100

    function: LeaseType

---
+++ contracts/OriginalLease.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract OriginalLease {
+
+    address public lessor;
+    address public lessee;
+    uint256 public date;
+
+    function LeaseType() public {
+        
+    }
+}
+
