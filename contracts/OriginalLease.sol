df186b2ff8efee0756e513f160d218dbb64c455a
chefking <chefking9000@protonmail.com>
2022-01-16 07:17:40 +0100

    leaseType: fixed lease

--- contracts/OriginalLease.sol
+++ contracts/OriginalLease.sol
@@ -1,18 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
+import "./SafeMath.sol";
 
 contract OriginalLease {
 
-    address public lessor;
-    address public lessee;
+    using SafeMath for uint;
+
+    address payable lessor;
+    address payable lessee;
     uint256 public startDate;
     uint256 public endDate;
+    
 
-    function LeaseType(address lessee, uint256 startDate, uint256 endDate, uint256 numOfDays) public {
+    function fixedLease(address lessee, uint256 startDate, uint256 endDate, uint256 numOfDays) public {
         startDate = now;
         endDate = now + (numOfDays * 182 days);
 
-        if 
+        if (lessee )
     }
 }
 
